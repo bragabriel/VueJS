cd6b201671711504849f7feb8559746f3b888248
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-24 15:59:17 -0300

    add files: Vue.JS

---
+++ Curso-VueJS/Aula04-Componentes_VueCLI/vue-project/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+/* 
+    Componentes
+
+É por meio de Componentes que podemos dividir o nosso layout em PaymentResponse;
+Cada componente tem a sua responsabilidade, por exemplo: um componente que é uma tabela, o outro é o rodapé;
+O projeto fica melhor separado;
+Cada um tem seu CSS e também os dados que manipula.
+ */
