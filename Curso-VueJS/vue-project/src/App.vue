48c65a5f8cbbadca8e99438719b7270ff11163e0
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-06-28 15:50:52 -0300

    feat: Lifecycle hooks

--- Curso-VueJS/vue-project/src/App.vue
+++ Curso-VueJS/vue-project/src/App.vue
@@ -2,17 +2,20 @@
     <div>
       <PrimeiroComponente/>
       <p>O dado de app é: {{testando}}</p>  
+      <LifeCycle/>
     </div>
 </template>
 
 
 <script>
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LifeCycle from './components/LifeCycle.vue'
 
   export default{
     name: 'App', //exportando o componente App
     components: {
-      PrimeiroComponente //exportando outros componentes
+      PrimeiroComponente, //exportando outros componentes
+      LifeCycle
     },  
     data(){
       return{
---
+++ Curso-VueJS/vue-project/src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Meu nome é: {{nome}}</h1>
+</template>
+
+<script>
+export default{
+    name: 'LifeCycle', //exportando o componente
+    data(){
+        return{
+            nome: 'Ainda não sei'
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.nome = 'Gabriel'
+        }, 1000)
+        
+    },
+    mounted(){
+        setTimeout(() => {
+            this.nome = 'Braga'
+        }, 2000)   
+    }
+}
+</script>
