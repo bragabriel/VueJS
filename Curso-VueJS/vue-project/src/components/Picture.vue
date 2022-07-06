7a28e892128d849166b24d616b9d1dbfb0acb0c8
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2022-07-06 15:49:42 -0300

    add: reutilização de componentes

--- Curso-VueJS/vue-project/src/components/Picture.vue
+++ Curso-VueJS/vue-project/src/components/Picture.vue
@@ -1,11 +1,17 @@
 <template>
     <img :src="avatar" :alt="descricao">
     <!-- :src  OU  v-bind:src   =    são a mesma coisa -->
+    <Reutilizacao/>
 </template> 
 
 <script>
+import Reutilizacao from './Reutilizacao.vue'
+
     export default{
         name: 'Picture',
+        components: {
+            Reutilizacao
+        },
         data(){
             return {
                 avatar: "/img/avatar.png",
